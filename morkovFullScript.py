c82d2250367eb7edaca882ca5a2f101781afd57e
Ania <41090467+aniaskudlarska@users.noreply.github.com>
2021-04-11 19:02:34 -0400

    Cleaning up a bit, resetting interpreter

--- morkovFullScript.py
+++ morkovFullScript.py
@@ -1,4 +1,7 @@
 import markovify
+
+
+filename = input("Type the name of the file as base")
 with open ('path2EnglishParsed.txt', encoding="utf8") as f:
     text = f.read()
 
@@ -6,3 +9,8 @@ model = markovify.Text(text)
 
 for i in range(50):
     print(model.make_sentence())
+
+
+#TODO: Make this modular, open different files, and print them
+#TODO: Format text, replace \n\
+
